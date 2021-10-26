22f2668e3d981e2d524723be677e528edaec87bf
Ben <benjamin.etheredge@gmail.com>
2021-10-26 22:19:08 +0000

    Wrapper changes

--- baseline.py
+++ baseline.py
@@ -1,4 +1,3 @@
-from typing import Deque
 import gym
 from stable_baselines3.common import callbacks
 from stable_baselines3.common.callbacks import EvalCallback
@@ -14,6 +13,8 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.env_checker import check_env
 from torchsummary import summary
 
+from wrappers import *
+
 class CustomCNN(BaseFeaturesExtractor):
     """
     :param observation_space: (gym.Space)
@@ -21,7 +22,7 @@ class CustomCNN(BaseFeaturesExtractor):
         This corresponds to the number of unit for the last layer.
     """
 
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 256):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 512):
         super(CustomCNN, self).__init__(observation_space, features_dim)
         # We assume CxHxW images (channels first)
         # Re-ordering will be done by pre-preprocessing or wrapper
@@ -30,7 +31,12 @@ class CustomCNN(BaseFeaturesExtractor):
         ic(observation_space.shape)
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=5, stride=2),
+            nn.Conv3d(n_input_channels, 32, kernel_size=(2, 3, 3), stride=1),
+            nn.LeakyReLU(),
+
+            nn.Flatten(1, 2),
+
+            nn.Conv2d(32, 32, kernel_size=5, stride=2),
             nn.LeakyReLU(),
             # nn.BatchNorm2d(64),
 
@@ -57,28 +63,12 @@ class CustomCNN(BaseFeaturesExtractor):
         self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.LeakyReLU())
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
-        #ic()
         x = observations
-        #ic(x.shape)
-        #x = self.sub_cnn(x)
-        #ic(x.shape)
-        #x = self.cnn(x)
-        #ic(x.shape)
-        #x = self.linear(x)
-        #ic(x.shape)
-        #return x
-        #ic(x.shape)
-        #x = x.view(x.shape[0], x.shape[2], x.shape[1], x.shape[3:])
-        #ic(x.shape)
         x = self.cnn(x)
         x = self.linear(x)
-        #ic(x.shape)
         
         #x = th.gather(x, 1, [0, 2, 4, 8, 16, 32, 48, 64, 80, 96, 112, 128])
-        #ic(x.shape)
         return x
-
-        #return self.linear(self.cnn(observations))
 
 
 from stable_baselines3.common.vec_env import VecTransposeImage
@@ -90,57 +80,19 @@ def create_env_fn(port):
     def env_fn():
         nonlocal port
         env = gym.make("deepracer_gym:deepracer-v0", port=port)
-        return Monitor(env)
+        env = Extractor(env)
+        #env = RewardRework(env)
+        env = Stacker(env)
+        env = Monitor(env)
+        return env
     return env_fn
 
 import numpy as np
-
-class stacker(gym.Wrapper):
-    def __init__(self, env, stack_size=4):
-        super(stacker, self).__init__(env)
-        ic(env.observation_space.shape)
-        self.frames = None
-        self.observation_space = gym.spaces.Box(low=0, high=255., shape=(4,120,160), dtype=np.uint8)
-
-    def reset(self):
-        obs = self.env.reset() 
-        self.frames = Deque([obs] * 4, maxlen=4)
-        state = np.squeeze(np.stack(self.frames), axis=1)
-        return state
-
-    def step(self, action):
-        obs = self.env.step(action)
-        self.frames.append(obs)
-        state = np.squeeze(np.stack(self.frames), axis=1)
-        ic(state.shape)
-        return state
+import time as t
 
 
-class RewardRework(gym.Wrapper):
-    def __init__(self, env, expected_steps=500):
-        super(RewardRework, self).__init__(env)
-        self.steps = 0
-        self.expected_steps = expected_steps
-
-    def step(self, action):
-        self.steps += 1
-        obs, _reward, done, info = self.env.step(action)
-        if done and _reward > 0.001:
-            reward = self.expected_steps / self.steps
-        elif done:
-            reward = -1
-        else:
-            reward = 0
-
-        return obs, reward, done, info
-
-    def reset(self):
-        obs = self.env.reset()
-        self.steps = 0
-        return obs
-
-N_ENVS = 28
-EVAL_N_ENVS = 8
+N_ENVS = 32
+EVAL_N_ENVS = 32
 PORT = 8888
 STACK_SIZE = 4
 from time import sleep
@@ -152,54 +104,30 @@ def main():
     env = SubprocVecEnv(
         [create_env_fn(PORT+idx) for idx in range(N_ENVS)])
     sleep(10)
-    #env = SubprocVecEnv([env_fn for _ in range(N_ENVS)])
-    ic(env.observation_space.shape)
-    #env = VecTransposeImage(env)
-    ic(env.observation_space.shape)
-    env = VecFrameStack(env, n_stack=STACK_SIZE, channels_order='first')
-    ic(env.observation_space.shape)
-    #env = VecNormalize(env, norm_obs=True, norm_reward=False)
-    #env = stacked_observations(env, n_stack=3)
     ic(env.observation_space.shape)
     # TODO pass through info for stacker
 
     eval_env = SubprocVecEnv(
-        [create_env_fn(PORT+idx+N_ENVS) for idx in range(EVAL_N_ENVS)])
-    #eval_env = SubprocVecEnv([env_fn for _ in range(EVAL_N_ENVS)])
+        [create_env_fn(PORT+idx) for idx in range(EVAL_N_ENVS)])
 
     ic(eval_env.observation_space.shape)
-    eval_env = VecFrameStack(eval_env, n_stack=STACK_SIZE, channels_order='first')
-    ic(eval_env.observation_space.shape)
-    #eval_env = VecNormalize(eval_env, norm_obs=True, norm_reward=False)
-    #eval_env = VecTransposeImage(eval_env)
-    ic(eval_env.observation_space.shape)
-    #rollout = (8192 * 1) // N_ENVS
+
     rollout = (8192*2) // N_ENVS
-    #rollout = 8192 // N_ENVS
-    #rollout = 256
+
     ev_call = EvalCallback(
         eval_env,
         #n_eval_episodes=max(10, EVAL_N_ENVS*4),
-        n_eval_episodes=EVAL_N_ENVS*4,
+        n_eval_episodes=EVAL_N_ENVS*1,
         eval_freq=(rollout*4),
         #log_path="./logs/",
-        best_model_save_path="submit_models/",
+        best_model_save_path=f"models/{t.time()}",
         deterministic=True,
     )
-    '''
-    ev2_call = EvalCallback(
-        eval_env,
-        n_eval_episodes=10,
-        eval_freq=5000,
-        log_path="./logs/",
-        best_model_save_path="./logs/",
-        deterministic=False,
-    )
-    '''
     policy_kwargs = dict(
         features_extractor_class=CustomCNN,
         features_extractor_kwargs=dict(features_dim=512),
-        net_arch=[dict(pi=[128, 128], vf=[128, 128])]
+        # TODO no shared feature extractor?
+        net_arch=[dict(pi=[256, 256], vf=[256, 256])]
         #features_extractor_kwargs=dict(
             #net_arch=[dict(
                 #pi=[]
